55c1c8f5522b32c3dd349d168bd0b678ac134116
Rishabhdevloper <144427674+Rishabhdevloper@users.noreply.github.com>
2024-07-02 20:36:34 +0530

    Time: 2 ms (73.92%), Space: 42.4 MB (31.25%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,48 +1,66 @@
+class Pair{
+    int row;
+    int col;
+    int time;
+    Pair(int _row,int _col,int _time){
+        this.row=_row;
+        this.col=_col;
+        this.time=_time;
+    }
+}
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int[][] visited = grid;
-        Queue<int[]> q = new LinkedList<>();
-        int countFreshOrange = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (visited[i][j] == 2) {
-                    q.offer(new int[] {i, j});
+        int n=grid.length;
+        int m=grid[0].length;
+        int visited[][]=new int[n][m];
+        Queue<Pair>pq=new LinkedList<>();
+        int cntfresh=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){
+                    visited[i][j]=2;
+                     pq.add(new Pair(i,j,0));
                 }
-                if (visited[i][j] == 1) {
-                    countFreshOrange++;
+                else{
+                    visited[i][j]=0;
+                }
+                if(grid[i][j]==1)cntfresh++;
+            }
+
+        }
+        int time=0;
+        int delr[]={-1,0,+1,0};
+        int delc[]={0,+1,0,-1};
+        int cnt=0;
+        while(!pq.isEmpty()){
+            int r=pq.peek().row;
+            int c=pq.peek().col;
+            int tm=pq.peek().time;
+            pq.remove();
+            time=Math.max(tm,time);
+           
+            for(int k=0;k<4;k++){
+                int numr=r+delr[k];
+                int numc=c+delc[k];
+                if(numr>=0 &&numr<n&&numc>=0&&numc<m&&visited[numr][numc]==0&&grid[numr][numc]==1){
+                    pq.add(new Pair(numr,numc,tm+1));
+                    visited[numr][numc]=2;
+                     cnt++;
                 }
             }
+
+
         }
-        if (countFreshOrange == 0)
-            return 0;
-        if (q.isEmpty())
-            return -1;
-        
-        int minutes = -1;
-        int[][] dirs = {{1, 0},{-1, 0},{0, -1},{0, 1}};
-        while (!q.isEmpty()) {
-            int size = q.size();
-            while (size-- > 0) {
-                int[] cell = q.poll();
-                int x = cell[0];
-                int y = cell[1];
-                for (int[] dir : dirs) {
-                    int i = x + dir[0];
-                    int j = y + dir[1];
-                    if (i >= 0 && i < m && j >= 0 && j < n && visited[i][j] == 1) {
-                        visited[i][j] = 2;
-                        countFreshOrange--;
-                        q.offer(new int[] {i, j});
-                    }
-                }
-            }
-            minutes++;
-        }
-        
-        if (countFreshOrange == 0)
-            return minutes;
-        return -1;
+        if(cntfresh!=cnt)return -1;
+        return time;
+
+
+           
+
+
+
+
+
+
     }
 }
