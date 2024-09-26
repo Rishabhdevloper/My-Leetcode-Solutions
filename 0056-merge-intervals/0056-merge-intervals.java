bb35d59845c1289463394b17395fcf8175c2ce52
Rishabhdevloper <144427674+Rishabhdevloper@users.noreply.github.com>
2024-09-27 00:03:24 +0530

    Time: 9 ms (51.46%), Space: 46.5 MB (42.37%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -5,7 +5,7 @@ class Solution {
         // Sort the intervals based on the starting times
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         
-        List<int[]> result = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
         int x = intervals[0][0];
         int y = intervals[0][1];
         
@@ -13,18 +13,31 @@ class Solution {
             // If the current interval overlaps with the previous one
             if (intervals[i][0] <= y) {
                 y = Math.max(y, intervals[i][1]);
-            } else {
-                // Add the merged interval to the result
-                result.add(new int[]{x, y});
+            }
+             else {
+                ArrayList<Integer>list=new ArrayList<>();
+                list.add(x);
+                list.add(y);
+                result.add(list);
                 // Update x and y to the current interval
                 x = intervals[i][0];
                 y = intervals[i][1];
             }
         }
+        ArrayList<Integer>pist=new ArrayList<>();
+        pist.add(x);
+        pist.add(y);
+        result.add(pist);
         // Add the last interval
-        result.add(new int[]{x, y});
+        
+        int ans[][]=new int[result.size()][2];
+        for(int i=0;i<result.size();i++){
+            ans[i][0]=result.get(i).get(0);
+            ans[i][1]=result.get(i).get(1);
+        }
+        return ans;
         
         // Convert the list of arrays into a 2D array and return
-        return result.toArray(new int[result.size()][]);
+        
     }
 }
