ec26eccb13cf5d8bc2d032d981ec4283754e2779
Rishabhdevloper <144427674+Rishabhdevloper@users.noreply.github.com>
2024-10-09 23:49:02 +0530

    Time: 6 ms (86.16%), Space: 42.5 MB (95.04%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -7,7 +7,8 @@ class Solution {
         for (int i = n - 1; i >= 0; i--) {
             if (s.charAt(i) != ' ') {
                 str.append(s.charAt(i));
-            } else if (s.charAt(i) == ' ' && str.length() > 0) {
+            } 
+            else if (s.charAt(i) == ' ' && str.length() > 0) {
                 str.reverse();
                 ans.append(str).append(" ");
                 str.setLength(0);  // Clear the StringBuilder for the next word
