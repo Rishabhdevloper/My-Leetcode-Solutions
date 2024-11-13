1318a082c717dfee16d7dbdd9f259bf97c142d9d
Rishabhdevloper <144427674+Rishabhdevloper@users.noreply.github.com>
2024-11-14 00:27:22 +0530

    Time: 2 ms (14.54%), Space: 41.6 MB (33.36%) - LeetHub

--- 0748-largest-number-at-least-twice-of-others/0748-largest-number-at-least-twice-of-others.java
+++ 0748-largest-number-at-least-twice-of-others/0748-largest-number-at-least-twice-of-others.java
@@ -3,16 +3,16 @@ class Solution {
         int maxi = Integer.MIN_VALUE;
         int maxi1 = Integer.MIN_VALUE;
         int index = -1;
-        for(int i = 0; i < nums.length; i++){
-            if(maxi < nums[i]){
+        for (int i = 0; i < nums.length; i++) {
+            if (maxi < nums[i]) {
                 maxi = nums[i];
                 index = i;
             }
         }
-        for(int i = 0; i < nums.length; i++){
-            if(maxi1 < nums[i]&&nums[i]!=maxi){
+        for (int i = 0; i < nums.length; i++) {
+            if (maxi1 < nums[i] && nums[i] < maxi) {
                 maxi1 = nums[i];
-                  
+
             }
         }
         System.out.print(maxi1);
