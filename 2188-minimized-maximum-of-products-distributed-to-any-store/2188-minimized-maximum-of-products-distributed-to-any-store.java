dc7b4e9536ff9129ba331ef49c1627585f93328e
Rishabhdevloper <144427674+Rishabhdevloper@users.noreply.github.com>
2024-11-15 01:36:10 +0530

    Time: 61 ms (42.81%), Space: 56.8 MB (39.97%) - LeetHub

---
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.java
@@ -0,0 +1,35 @@
+class Solution {
+    public static boolean f(int[] quantities, int mid, int n) {
+    int count = 0;
+    for (int quantity : quantities) {
+        count += (quantity + mid - 1) / mid; // this rounds up without needing Math.ceil
+        if(count<0)return false;
+    }
+    return count <= n;
+}
+
+    public int minimizedMaximum(int n, int[] quantities) {
+       int m=quantities.length;
+       int sum=0;
+      Arrays.sort(quantities);
+       for(int i=0;i<m;i++){
+         sum+=quantities[i];
+       }
+       int start=1;
+       int end=quantities[m-1];
+        int ans=Integer.MAX_VALUE;
+       while(start<=end){
+        int mid=(start+end)/2;
+       
+        if(f(quantities,mid,n)){
+            ans=Math.min(ans,mid);
+            end=mid-1;
+        }
+        else{
+            start=mid+1;
+        }
+       }
+       return ans;
+
+    }
+}
