c80cb91c26c906c9e48593f2b1d2ba2efd7e3789
Rishabhdevloper <144427674+Rishabhdevloper@users.noreply.github.com>
2024-12-03 22:57:46 +0530

    Time: 1 ms (99.81%), Space: 45.3 MB (53.97%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
@@ -6,10 +6,7 @@ class Solution {
             return Integer.MAX_VALUE;
         }
         if (row == matrix.length - 1) {
-            if (col < matrix[0].length && col >= 0)
-                return matrix[row][col];
-            else 
-                return 0;
+           return matrix[row][col];
         }
         if (dp[row][col] != Integer.MIN_VALUE) {
             return dp[row][col];
