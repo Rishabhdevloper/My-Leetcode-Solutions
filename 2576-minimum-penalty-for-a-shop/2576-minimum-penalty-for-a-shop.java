d5aef3edb2f0ccf39b4b6ae9b95f27256b359b8e
Rishabhdevloper <144427674+Rishabhdevloper@users.noreply.github.com>
2024-12-04 22:48:10 +0530

    Time: 11 ms (71.19%), Space: 45.2 MB (53.43%) - LeetHub

--- 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.java
+++ 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.java
@@ -1,31 +1,31 @@
 class Solution {
     public int bestClosingTime(String customers) {
         int n=customers.length();
-        int arr[]=new int[n+1];
+        
         int cnt=0;
         for(int i=0;i<n;i++){
            if(customers.charAt(i)=='N')cnt++;
         }
-        arr[n]=cnt;
+        int ans=n;
+        int mpenality=cnt;
         for(int i=n-1;i>=0;i--){
             if(customers.charAt(i)=='Y'){
                 cnt++;
-                arr[i]=cnt;
+                if(mpenality>=cnt){
+                    ans=i;
+                    mpenality=cnt;
+
+                }
             }
             else{
                 cnt--;
-                arr[i]=cnt;
-            }
-        }
-        int ans=0;//hours
-        int minpenality=Integer.MAX_VALUE;
-        for(int i=0;i<arr.length;i++){
-            if(minpenality>arr[i]){
-                minpenality=arr[i];
-                ans=i;
-            }
-        }
+                if(mpenality>=cnt){
+                    ans=i;
+                    mpenality=cnt;
 
-     return ans;
+                }
+            }
+        }
+        return ans;
     }
 }
