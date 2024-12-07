4abecd24229efefb46724574493b5a9ae1500605
Rishabhdevloper <144427674+Rishabhdevloper@users.noreply.github.com>
2024-12-08 01:35:50 +0530

    Time: 31 ms (72.49%), Space: 60.5 MB (17.96%) - LeetHub

--- 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.java
+++ 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.java
@@ -1,24 +1,16 @@
 class Solution {
-    public int minimumSize(int[] nums, int maxOperations) {
-         int maxi=Integer.MIN_VALUE;
-         int n=nums.length;
-         for(int i=0;i<n;i++){
-             maxi=Math.max(nums[i],maxi);
-         }
-         int low=1;
-         int high=maxi;
-         while(low<=high){
-            int mid=(low+high)/2;
-            int og=0;
-            for(int i:nums){
-                //Iska mtlb kitne operations mein nums[i]ko mid bna dengne
-                    og+=(i-1)/mid;
+    public int minimumSize(int[] nums, int maxOps) {
+        int low = 1, high = Arrays.stream(nums).max().getAsInt();
+        System.out.println(high);
+        while (low < high) {
+            int mid = (low + high) / 2;
+            int ops = 0;
+            for (int n : nums) {
+                ops += (n - 1) / mid;
             }
-            if(og<=maxOperations)high=mid-1;
-            else {
-                low=mid+1;
-            }
-         }
-         return low;
+            if (ops <= maxOps) high = mid;
+            else low = mid + 1;
+        }
+        return high;
     }
 }
