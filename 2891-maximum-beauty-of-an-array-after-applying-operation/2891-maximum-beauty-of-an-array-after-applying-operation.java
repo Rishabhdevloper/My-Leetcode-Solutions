dff194b7003636942ba29c25e496fe299b6d45f1
Rishabhdevloper <144427674+Rishabhdevloper@users.noreply.github.com>
2024-12-13 02:34:46 +0530

    Time: 156 ms (8.03%), Space: 61.2 MB (17.42%) - LeetHub

--- 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.java
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.java
@@ -1,28 +1,23 @@
 class Solution {
     public int maximumBeauty(int[] nums, int k) {
-        if (nums.length == 1)
-            return 1;
+        int n=nums.length;
+        int arr[][]=new int[n][2];
+        for(int i=0;i<n;i++){
+             arr[i][0]=nums[i]-k;
+             arr[i][1]=nums[i]+k;
 
+        }
+        Arrays.sort(arr,(a,b)->Integer.compare(a[0],b[0]));
         int maxBeauty = 0;
-        int maxValue = 0;
-
-        for (int num : nums)
-            maxValue = Math.max(maxValue, num);
-
-        int[] count = new int[maxValue + 1];
-
-        for (int num : nums) {
-            count[Math.max(num - k, 0)]++;
-            count[Math.min(num + k + 1, maxValue)]--;
+        Queue<Integer> queue = new LinkedList<>();
+        for(int val[]:arr){
+            while(!queue.isEmpty()&&queue.peek()<val[0]){
+                queue.poll();
+            }
+            queue.add(val[1]);
+            maxBeauty=Math.max(maxBeauty,queue.size());
         }
-
-        int currentSum = 0;
-
-        for (int val : count) {
-            currentSum += val;
-            maxBeauty = Math.max(maxBeauty, currentSum);
-        }
-
         return maxBeauty;
+      
     }
 }
