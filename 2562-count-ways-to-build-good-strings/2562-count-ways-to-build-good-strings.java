a3b27686df35a4f9896de3e2df43ca1c2989e580
Rishabhdevloper <144427674+Rishabhdevloper@users.noreply.github.com>
2024-12-31 12:53:20 +0530

    Time: 12 ms (31.88%), Space: 56.8 MB (17.78%) - LeetHub

--- 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.java
+++ 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.java
@@ -1,15 +1,29 @@
 class Solution {
     int mod=1000000007;
-   
     public int f(int low, int high, int zero, int one,int len,int dp[]){
            if(len>high)return 0;
+           int cntzero=len+zero;
+           int cntone=len+one;
            if(dp[len]!=-1)return dp[len];
-           int ans=f(low,  high,  zero,  one,len+zero,dp)+f(low,  high,  zero,  one,len+one,dp);
-           if(len>=low&&len<=high)ans++;
+           if(cntzero>=low&&cntzero<=high){
+            cntzero=1;
+           }
+           else{
+            cntzero=0;
+           }
+           if(cntone>=low&&cntone<=high){
+            cntone=1;
+           }
+           else{
+            cntone=0;
+           }
+
+           int ans=cntzero+f(low,  high,  zero,  one,len+zero,dp)+cntone+f(low,  high,  zero,  one,len+one,dp);
            return dp[len]=ans%mod;
     }
     public int countGoodStrings(int low, int high, int zero, int one) {
         int len=0;
+       
         int dp[]=new int[high+1];
         Arrays.fill(dp,-1);
       return  f(low,high,zero,one,len,dp); 
