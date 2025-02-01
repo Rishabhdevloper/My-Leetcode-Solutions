5c1816e630c652369376b5c4a1d4b9311f85547b
Rishabhdevloper <144427674+Rishabhdevloper@users.noreply.github.com>
2025-02-01 12:36:36 +0530

    Time: 0 ms (100%), Space: 43.1 MB (84.31%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.java
+++ 3429-special-array-i/3429-special-array-i.java
@@ -1,5 +1,15 @@
 class Solution {
     public boolean isArraySpecial(int[] nums) {
-        
+        int n=nums.length;
+        if(n==1){
+            return true;
+        }
+        for(int i=1;i<nums.length;i++){
+         if((nums[i]%2==0&&nums[i-1]%2==0)||(nums[i]%2==1&&nums[i-1]%2==1))return false;
+
+
+
+        }
+        return true;
     }
 }
