7864e1cf279afd2750a7e7c77cc0cb4bffeb0b48
Rishabhdevloper <144427674+Rishabhdevloper@users.noreply.github.com>
2025-02-08 00:30:56 +0530

    Time: 15 ms (64.84%), Space: 44.3 MB (45.05%) - LeetHub

---
+++ 1844-maximum-number-of-balls-in-a-box/1844-maximum-number-of-balls-in-a-box.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countBalls(int lowLimit, int highLimit) {
+    int maxi=Integer.MIN_VALUE;
+    int arr[]=new int[99999];
+    for(int i=lowLimit;i<=highLimit;i++){
+        int n=i;
+        int sum=0;
+        while(n>0){
+            sum+=(n%10);
+            n=n/10;
+        }
+        arr[sum]+=1;
+        maxi=Math.max(arr[sum],maxi);
+    }
+       return maxi; 
+    }
+}
