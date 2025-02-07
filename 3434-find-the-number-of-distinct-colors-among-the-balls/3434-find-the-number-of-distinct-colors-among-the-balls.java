5fa729d799533a3e3fb35e5573a806014fed49a9
Rishabhdevloper <144427674+Rishabhdevloper@users.noreply.github.com>
2025-02-08 00:58:40 +0530

    Time: 40 ms (72.05%), Space: 99.2 MB (25.33%) - LeetHub

--- 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.java
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 class Solution {
     public int[] queryResults(int limit, int[][] queries) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        HashMap<Integer, Integer> countMap = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();//Ball-Colour
+        HashMap<Integer, Integer> countMap = new HashMap<>();//Colour-Count
         int n = queries.length;
         int[] ans = new int[n];
         int uniqueCount = 0;
@@ -16,18 +16,15 @@ class Solution {
                 int prevValue = map.get(x);
                 countMap.put(prevValue, countMap.get(prevValue) - 1);
                 if (countMap.get(prevValue) == 0) {
-                    uniqueCount--;
+                    countMap.remove(prevValue);
                 }
             }
 
             map.put(x, y);
             countMap.put(y, countMap.getOrDefault(y, 0) + 1);
 
-            if (countMap.get(y) == 1) {
-                uniqueCount++;
-            }
 
-            ans[i] = uniqueCount;
+            ans[i] = countMap.size();
         }
 
         return ans;
