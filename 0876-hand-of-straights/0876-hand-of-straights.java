281ca6b4f5e32b50a9daab0852a1c692529f018f
Rishabhdevloper <144427674+Rishabhdevloper@users.noreply.github.com>
2025-02-14 00:10:05 +0530

    Time: 63 ms (26.81%), Space: 45.4 MB (79.46%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.java
+++ 0876-hand-of-straights/0876-hand-of-straights.java
@@ -1,31 +1,25 @@
-import java.util.*;
-
 class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
-        if (hand.length % groupSize != 0) return false;
-
-        TreeMap<Integer, Integer> countMap = new TreeMap<>();
-        
-        // Count occurrences of each number
-        for (int num : hand) {
-            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+       int n=hand.length;
+       if((n%groupSize)!=0)return false;
+       TreeMap<Integer,Integer>map=new TreeMap<>();
+        for(int i=0;i<hand.length;i++){
+            map.put(hand[i],map.getOrDefault(hand[i],0)+1);
         }
-        
-        while (!countMap.isEmpty()) {
-            int first = countMap.firstKey();  
-            
-            
-            for (int i = 0; i < groupSize; i++) {
-                int num = first + i;
-                if (!countMap.containsKey(num)) return false;  
-                
-                countMap.put(num, countMap.get(num) - 1);
-                if (countMap.get(num) == 0) {
-                    countMap.remove(num);
+        while(map.size()>0){
+            int first=map.firstKey();
+            for(int i=0;i<groupSize;i++){
+                int num=first+i;
+                if(!map.containsKey(num)){
+                   return false;
+                }
+                map.put(num,map.get(num)-1);
+                if(map.get(num)==0){
+                    map.remove(num);
                 }
             }
         }
-        
         return true;
+
     }
 }
