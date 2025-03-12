96a8ec66ca7d38786e1f0e3e1f34d9721c31627f
Rishabhdevloper <144427674+Rishabhdevloper@users.noreply.github.com>
2025-03-12 18:33:02 +0530

    Time: 0 ms (100%), Space: 45.2 MB (25.27%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.java
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.java
@@ -5,11 +5,48 @@ class Solution {
         int high=n-1;
         int neg=0;
          int pos=0;
-        for(int i=0;i<n;i++){
-           if(nums[i]<0)neg++;
-           if(nums[i]>0)pos++;
-        }
-        int maxi=Math.max(neg,pos);
-        return maxi;
+         //Binary search for positive value
+         int ans=-1;
+         int que=-1;
+         while(low<=high){
+             int mid=(low+high)/2;
+             if(nums[mid]>0){
+                ans=mid;
+                high=mid-1;
+             }
+             else{
+                low=mid+1;
+             }
+         }
+         int cntp=0;
+         int cntn=0;
+         
+         if(ans!=-1){
+            cntp=n-ans;
+         }
+         else{
+            cntp=0;
+         }
+         low=0;
+         high=n-1;
+          while(low<=high){
+             int mid=(low+high)/2;
+             if(nums[mid]<0){
+                que=mid;
+               low=mid+1;
+             }
+             else{
+                high=mid-1;
+             }
+         }
+         if(que!=-1){
+            cntn=que+1;
+         }
+         else{
+            que=0;
+         }
+         
+         int maxi=Math.max(cntp,cntn);
+         return maxi;
     }
 }
