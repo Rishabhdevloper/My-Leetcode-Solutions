8751cf19608c212e2a5638f44e49bb7b04ae1562
Rishabhdevloper <144427674+Rishabhdevloper@users.noreply.github.com>
2025-03-13 01:16:17 +0530

    Time: 0 ms (100%), Space: 40.6 MB (99.99%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+     public TreeNode invertTree(TreeNode root) {
+        if(root==null)return root;
+        if(root.left==null && root.right==null)return root;
+        TreeNode l=root.left;
+        TreeNode r=root.right;
+        root.left=invertTree(r);
+        root.right=invertTree(l);
+        return root;
+        
+        
+    }
+        public boolean isSameTree(TreeNode p, TreeNode q) {
+        // Base case: if both trees are null, they are identical
+        if (p == null && q == null) {
+            return true;
+        }
+        // If only one tree is null or the values are different, they are not identical
+        if (p == null || q == null || p.val != q.val) {
+            return false;
+        }
+        // Recursively check if the left and right subtrees are identical
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+       invertTree(root.left);
+      return isSameTree(root.left,root.right);
+
+        
+    }
+}
