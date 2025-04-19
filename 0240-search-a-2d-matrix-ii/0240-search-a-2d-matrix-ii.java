1a7b40f91c07eefd19c0a4df4e4445cb6a28930f
RISHABH SONI <144427674+Rishabhdevloper@users.noreply.github.com>
2025-04-19 23:10:00 +0530

    Time: 7 ms (20.34%), Space: 46.1 MB (58.33%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -3,13 +3,20 @@ class Solution {
         int n=matrix.length;
         int m=matrix[0].length;
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]==target){
-                    return true;
-
-                }
+           int start=0;
+           int end=m-1;
+           while(start<=end){
+            int mid=(start+end)/2;
+            if(matrix[i][mid]==target)return true;
+            else if(matrix[i][mid]>target){
+                end=mid-1;
             }
-        }
+            else{
+                start=mid+1;
+            }
+           }
+            }
+        
         return false;
     }
 }
