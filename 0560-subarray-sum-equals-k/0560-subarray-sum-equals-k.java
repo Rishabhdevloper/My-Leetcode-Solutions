c71dc2cb355f158533a671f3f1decdf6195c44ba
RISHABH SONI <144427674+Rishabhdevloper@users.noreply.github.com>
2025-04-22 02:05:37 +0530

    Time: 19 ms (99.06%), Space: 47.6 MB (5.38%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,27 +1,23 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-          Map<Integer, Integer> map = new HashMap<>();
-        int sum = 0;
-        int N=nums.length;
-        int count=0;
+        HashMap<Integer,Integer>map=new HashMap<>();
         map.put(0,1);
-        for (int i = 0; i < N; i++) {
-            //calculate the prefix sum till index i:
-            sum += nums[i];
-
-            // calculate the sum of remaining part i.e. x-k:
-            int remove = sum - k;
-
-            //Calculate the length and update maxLen:
-           count += map.getOrDefault(remove, 0);
-
-            // Update the count of prefix sum
-            // in the map.
-            map.put(sum, map.getOrDefault(sum, 0) + 1);
-            //Finally, update the map checking the conditions:
-          
+        int n=nums.length;
+        int pref[]=new int[n];
+        pref[0]=nums[0];
+        for(int i=1;i<nums.length;i++){
+            pref[i]=pref[i-1]+nums[i];
         }
-        return count;
+        int cnt=0;
+        for(int i=0;i<n;i++){
+                if(map.containsKey(pref[i]-k)){
+                   cnt+=map.get(pref[i]-k);
+                }
+                
+            
+            map.put(pref[i],map.getOrDefault(pref[i],0)+1);
+        }
 
+        return cnt;
     }
-    }
+}
