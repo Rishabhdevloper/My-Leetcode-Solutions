0d7abe5cec44f34984b30091bdbb95e324f0a35e
RISHABH SONI <144427674+Rishabhdevloper@users.noreply.github.com>
2025-04-27 21:02:32 +0530

    Time: 23 ms (66.48%), Space: 48.2 MB (39.74%) - LeetHub

--- 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.java
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.java
@@ -1,32 +1,28 @@
-public class Solution {
+class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-        // Initialize count of subarrays, prefix sum, and hash map for remainders
-        int count = 0;
-        int prefixSum = 0;
-        HashMap<Integer, Integer> prefixMap = new HashMap<>();
-        prefixMap.put(0, 1); // To handle subarrays that start from the beginning
+      HashMap<Integer,Integer>map=new HashMap<>();
+      map.put(0,1);
+      int cnt=0;
+      int n=nums.length;
+      int sm=0;
+      for(int i=0;i<n;i++){
+         sm+=nums[i];
+         int mod=sm%k;
+         if(mod<0){
+            mod+=k;
+         }
+         if(map.containsKey(mod)){
+            cnt+=map.get(mod);
+            map.put(mod,map.get(mod)+1);
+         }
+         else{
+            map.put(mod,1);
+         }
 
-        for (int num : nums) {
-            // Update prefix sum
-            prefixSum += num;
-            
-            // Calculate the remainder of the prefix sum divided by k
-            int mod = prefixSum % k;
-            
-            // Adjust negative remainders to be positive
-            if (mod < 0) {
-                mod += k;
-            }
-            
-            // If this remainder has been seen before, it means there are subarrays ending here that are divisible by k
-            if (prefixMap.containsKey(mod)) {
-                count += prefixMap.get(mod);
-                prefixMap.put(mod, prefixMap.get(mod) + 1);
-            } else {
-                prefixMap.put(mod, 1);
-            }
-        }
-        
-        return count; // Total number of subarrays divisible by k
+
+      }
+      
+
+    return cnt;
     }
 }
