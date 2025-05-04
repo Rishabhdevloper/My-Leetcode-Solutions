71e6f987ea86690e9a3b4ab6f66d9a9dadf0fdfe
RISHABH SONI <144427674+Rishabhdevloper@users.noreply.github.com>
2025-05-04 17:11:24 +0530

    Time: 4 ms (75.57%), Space: 52.6 MB (91.09%) - LeetHub

--- 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.java
+++ 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.java
@@ -1,16 +1,14 @@
 class Solution {
     public int numEquivDominoPairs(int[][] dominoes) {
-        int n=dominoes.length;
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            int x=dominoes[i][0];
-            int y=dominoes[i][1];
-            for(int j=i+1;j<n;j++){
-                int k=dominoes[j][0];
-                int l=dominoes[j][1];
-                if((x==k&&y==l)||(x==l&&y==k))cnt++;
-            }
+        int[] count = new int[100];  
+        int ans = 0;
+        for (int[] domino : dominoes) {
+            int x = domino[0];
+            int y = domino[1];
+            int key = Math.min(x, y) * 10 + Math.max(x, y);
+            ans += count[key];  
+            count[key]++;
         }
-        return cnt;
+        return ans;
     }
 }
