bffc35a97d88a01575eba94d0ace95fe48814bdf
RISHABH SONI <144427674+Rishabhdevloper@users.noreply.github.com>
2025-05-13 17:16:45 +0530

    Time: 257 ms (24.05%), Space: 45.7 MB (41.77%) - LeetHub

--- 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.java
+++ 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.java
@@ -1,34 +1,31 @@
 class Solution {
     public int lengthAfterTransformations(String s, int t) {
-        int MOD = 1_000_000_007;
+        int MOD = 1000000007;
 
-       
         long[] freq = new long[26];
         for (char ch : s.toCharArray()) {
             freq[ch - 'a']++;
         }
+       while(t-->0){
+          long next[]=new long[26];
+          for(int i=0;i<26;i++){
+             if(i==25){
+                 next[0]=(next[0]+freq[i])%MOD;
+                 next[1]=(next[1]+freq[i])%MOD;
 
+             }
+             else{
+                next[i+1]=(next[i+1]+(freq[i]))%MOD;
+             }
+          }
+          freq=next;
+       }
+       long total=0;
+       for(int i=0;i<26;i++){
+            total=(total+freq[i])%MOD;
+       }
        
-        for (int i = 0; i < t; i++) {
-            long[] next = new long[26];
-
-            for (int j = 0; j < 26; j++) {
-                if (j == 25) { // 'z'
-                    next[0] = (next[0] + freq[j]) % MOD; // 'a'
-                    next[1] = (next[1] + freq[j]) % MOD; // 'b'
-                } 
-                else {
-                    next[j + 1] = (next[j + 1] + freq[j]) % MOD;
-                }
-            }
-            freq = next;
-        }
-
-        
-        long total = 0;
-        for (int i = 0; i < 26; i++) {
-            total = (total + freq[i]) % MOD;
-        }
+       
 
         return (int) total;
     }
